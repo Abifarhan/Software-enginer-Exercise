1d2760d89100a4b18c933e6b27933b0f9266ff61
abiFarhan <abifarhan04@gmail.com>
2021-11-11 21:06:08 +0700

    understand part 1

--- section7hash/index.js
+++ section7hash/index.js
@@ -8,14 +8,28 @@ class HashTable {
     let hash = 0;
     for (let i = 0; i < key.length; i++) {
       hash = (hash + key.charCodeAt(i) * i) % this.data.length;
-      console.log(hash);
+      // console.log(hash);
     }
     return hash;
   }
 
-  set(key, value) {}
+  set(key, value) {
+    let setValue  = 1
+
+    var a = this._hash(key)
+    value = a 
+    // console.log(value)
+    return value
+  }
+
+  get(){
+    var returnValue = this.set
+  }
 }
 
 const myHashTable = new HashTable(50);
-var testing1 = myHashTable._hash("grapes");
-console.log(testing1);
+// var testing1 = myHashTable._hash("grapes");
+// console.log(testing1);
+var a = myHashTable.set('grapes')
+var total = myHashTable.get
+console.log(total)
