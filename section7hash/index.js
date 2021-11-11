5678d6b3a1dbb5f92c60dc12c844cbfd156654fa
abiFarhan <abifarhan04@gmail.com>
2021-11-11 20:53:20 +0700

    Indentation code

--- section7hash/index.js
+++ section7hash/index.js
@@ -1,21 +1,21 @@
 class HashTable {
-    constructor(size){
-      this.data = new Array(size);
-    }
-  
-    _hash(key) { // _ meaning the object will be private
-      let hash = 0;
-      for (let i =0; i < key.length; i++){
-          hash = (hash + key.charCodeAt(i) * i) % this.data.length
-      }
-      return hash;
-    }
-
-    getFunction = get(_hash())
+  constructor(size) {
+    this.data = new Array(size);
   }
-  
 
-  function get(p1){
-      console.log("Anda masukkan ", p1)
+  _hash(key) {
+    // _ meaning the object will be private
+    let hash = 0;
+    for (let i = 0; i < key.length; i++) {
+      hash = (hash + key.charCodeAt(i) * i) % this.data.length;
+      console.log(hash);
+    }
+    return hash;
   }
-  const myHashTable = new HashTable(50)
+
+  set(key, value) {}
+}
+
+const myHashTable = new HashTable(50);
+var testing1 = myHashTable._hash("grapes");
+console.log(testing1);
