9f69224988cee17a72e56b6b3704665f5943c357
abiFarhan <abifarhan04@gmail.com>
2021-12-28 14:19:04 +0700

    adding add vertex

--- section10/index.js
+++ section10/index.js
@@ -8,11 +8,12 @@ class Graph{
     }
 
     addVertex(node){
-
+        this.adjacentList[node] = []
+        this.numberOfNodes++
     }
 
     addEdge(node1, node2){
-
+        
     }
 
     showConnections(){
@@ -30,3 +31,29 @@ class Graph{
         }
     }
 }
+
+const myGraph = new Graph()
+
+// adding vertex or node
+myGraph.addVertex('0')
+myGraph.addVertex('1')
+myGraph.addVertex('2')
+myGraph.addVertex('3')
+myGraph.addVertex('4')
+myGraph.addVertex('5')
+myGraph.addVertex('6')
+
+// give connection between vertex/node
+myGraph.addEdge('3', '1'); 
+myGraph.addEdge('3', '4'); 
+myGraph.addEdge('4', '2'); 
+myGraph.addEdge('4', '5'); 
+myGraph.addEdge('1', '2'); 
+myGraph.addEdge('1', '0'); 
+myGraph.addEdge('0', '2'); 
+myGraph.addEdge('6', '5');
+
+console.log(myGraph.showConnections)
+myGraph.showConnections()
+
+
